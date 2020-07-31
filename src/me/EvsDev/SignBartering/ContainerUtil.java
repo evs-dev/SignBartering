10ada3127ee3e3a5986bb160b39180bcc30c510d
Evan <evansmeath@gmail.com>
2020-07-31 17:29:01 +0100

    Removed and deprecated unused methods

--- src/me/EvsDev/SignBartering/ContainerUtil.java
+++ src/me/EvsDev/SignBartering/ContainerUtil.java
@@ -1,20 +1,12 @@
 package me.EvsDev.SignBartering;
 
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Sign;
-import org.bukkit.block.data.BlockData;
-import org.bukkit.block.data.Directional;
 import org.bukkit.inventory.InventoryHolder;
 
 public class ContainerUtil {
-
-	@Deprecated
-	public static final boolean isMaterialContainer(Material material) {
-		return false;
-	}
 	
 	public static final boolean isBlockStateContainer(BlockState blockState) {
 		return blockState instanceof InventoryHolder;
@@ -36,12 +28,5 @@ public class ContainerUtil {
 			}
 		}
 		return null;
-	}
-	
-	public static Block getInFrontBlock(Block block) {
-		BlockData data = block.getBlockData();
-        Directional directional = (Directional)data;
-        return block.getRelative(directional.getFacing());
-	}
-	
+	}	
 }
