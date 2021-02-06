b1a07f58145765a27f50bf5fb9df0388bbfd117c
evs-dev <evansmeath@gmail.com>
2021-02-06 15:11:33 +0000

    Fixed warnings in BarteringSign

--- src/me/EvsDev/SignBartering/BarteringSign.java
+++ src/me/EvsDev/SignBartering/BarteringSign.java
@@ -1,18 +1,15 @@
 package me.EvsDev.SignBartering;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
-@SuppressWarnings("all")
 public class BarteringSign {
 
     public BarteringSign(Sign sign) throws BarteringSignCreationException {
         if (!LineChecker.perfectFirstLine(sign.getLine(0))) throw new BarteringSignCreationException();
-        this.sign = sign;
-        this.location = sign.getLocation();
+        //this.sign = sign; Might be used later
         sellingItemStack = LineChecker.parseItemAndQuantityLine(sign.getLine(1), SBUtil.formattedItemQuantitySeparator);
         if (sellingItemStack == null) throw new BarteringSignCreationException(Errors.INVALID_SELLING_SIGN);
         priceItemStack = LineChecker.parseItemAndQuantityLine(sign.getLine(2), SBUtil.formattedItemQuantitySeparator);
@@ -21,8 +18,7 @@ public class BarteringSign {
         if (signOwner == null) throw new BarteringSignCreationException(Errors.INVALID_SELLING_SIGN);
     }
 
-    private final Sign sign;
-    private final Location location;
+    //private final Sign sign;
     private final ItemStack sellingItemStack;
     private final ItemStack priceItemStack;
     private final Player signOwner;
@@ -54,14 +50,17 @@ public class BarteringSign {
         return Bukkit.getPlayer(ownerName);
     }
 
+    @SuppressWarnings("serial")
     public class BarteringSignCreationException extends Exception {
         public BarteringSignCreationException() {
             this.error = Errors.NONE;
         }
+
         public BarteringSignCreationException(Errors error) {
             super(error.getErrorMessage());
             this.error = error;
         }
+
         public BarteringSignCreationException(Errors lineError, int line) {
             super(String.format(lineError.getErrorMessage(), line));
             this.error = lineError;
