6fa8d1aee98c8dfb872720a5a35c706561fe9ac2
evs-dev <evansmeath@gmail.com>
2021-02-07 17:20:20 +0000

    Add default line formatting classes

---
+++ src/me/EvsDev/SignBartering/LineFormatters/BasicNameLineFormatter.java
@@ -0,0 +1,22 @@
+package me.EvsDev.SignBartering.LineFormatters;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import me.EvsDev.SignBartering.SBUtil;
+import net.md_5.bungee.api.ChatColor;
+
+public class BasicNameLineFormatter extends NameLineFormatter {
+
+    @Override
+    public String formatPlayerFormattedLine(Player signMaker) {
+        return ChatColor.GRAY + "(" + signMaker.getDisplayName() + ")";
+    }
+
+    @Override
+    public Player interpretPluginFormattedLine(String line) {
+        String ownerName = SBUtil.removeBracketsFromSignLine(line);
+        return Bukkit.getPlayer(ownerName);
+    }
+
+}
---
+++ src/me/EvsDev/SignBartering/LineFormatters/BasicSellingLineFormatter.java
@@ -0,0 +1,32 @@
+package me.EvsDev.SignBartering.LineFormatters;
+
+import org.bukkit.inventory.ItemStack;
+
+import me.EvsDev.SignBartering.LineChecker;
+import me.EvsDev.SignBartering.Main;
+import me.EvsDev.SignBartering.SBUtil;
+
+public class BasicSellingLineFormatter extends SellingLineFormatter {
+
+    @Override
+    public Object interpretPlayerFormattedLine(String line) {
+        return LineChecker.parseItemAndQuantityLine(line, Main.ITEM_QUANTITY_SEPARATOR);
+    }
+
+    @Override
+    public boolean isValid(Object result) {
+        return result != null;
+    }
+
+    @Override
+    public String formatSelfInterpretedLine(Object result) {
+        ItemStack itemStack = (ItemStack) result;
+        return SBUtil.formatItemStackLine(itemStack);
+    }
+
+    @Override
+    public ItemStack interpretPluginFormattedLine(String line) {
+        return LineChecker.parseItemAndQuantityLine(line, Main.FORMATTED_ITEM_QUANTITY_SEPARATOR);
+    }
+
+}
